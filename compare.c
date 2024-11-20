26cea7ccebc362a003c7627925fc273ad9184380
LuizHenrique111 <luizhenriquecjz2007@gmail.com>
2024-11-20 17:36:21 +0000

    Wed, Nov 20, 2024, 2:36 PM -03:00

---
+++ compare.c
@@ -0,0 +1,12 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int x = get_int("What's X? ")
+    int y = get_int("What's Y? ")
+    if (x<y)
+    {
+        printf("X is less than Y \n")
+    }
+}
