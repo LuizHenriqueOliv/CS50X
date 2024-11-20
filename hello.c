50c151454b5063f600077348ec9172c6cd95446b
LuizHenrique111 <luizhenriquecjz2007@gmail.com>
2024-11-20 17:17:37 +0000

    Wed, Nov 20, 2024, 2:17 PM -03:00

--- hello.c
+++ hello.c
@@ -3,7 +3,6 @@
 
 int main(void)
 {
-    string nome = get_string("Qual o seu nome? ");
-    string idade = get_string("Qual a sua idade? ");
-    printf("Olá, %s, você tem %s anos \n", nome, idade);
+    string answer = get_string("Qual o seu nome? ");
+    printf("Olá, %s \n", answer);
 }
