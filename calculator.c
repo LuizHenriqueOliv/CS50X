5293083de954ab974e78d4a519647906edd4f337
LuizHenrique111 <luizhenriquecjz2007@gmail.com>
2024-11-21 17:18:27 +0000

    Thu, Nov 21, 2024, 2:18 PM -03:00

--- calculator.c
+++ calculator.c
@@ -1,9 +1,17 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int add(int a, int b);
 int main(void)
 {
     int x = get_int("X: ");
     int y = get_int("Y: ");
-    printf("%i\n", x + y);
+
+    int z = add(x, y);
+    printf("%i\n", z);
+}
+
+int add(int a, int b)
+{
+    return a + b;
 }
