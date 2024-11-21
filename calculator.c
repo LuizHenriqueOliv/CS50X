eae168acb2f1d47faa335547d0098ff8f22e200c
LuizHenrique111 <luizhenriquecjz2007@gmail.com>
2024-11-21 19:29:27 +0000

    Thu, Nov 21, 2024, 4:29 PM -03:00

--- calculator.c
+++ calculator.c
@@ -1,16 +1,11 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int add(int a, int b);
 int main(void)
 {
     int x = get_int("X: ");
     int y = get_int("Y: ");
 
-    printf("%i\n", add(x, y));
+    printf("%i\n", x / y);
 }
 
-int add(int a, int b)
-{
-    return a + b;
-}
