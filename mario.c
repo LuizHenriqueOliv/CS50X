a03646638306c4859b02ba83112c359de250fc53
LuizHenrique111 <luizhenriquecjz2007@gmail.com>
2024-11-21 19:21:13 +0000

    Thu, Nov 21, 2024, 4:21 PM -03:00

--- mario.c
+++ mario.c
@@ -3,13 +3,15 @@
 
 int main(void)
 {
+    // promp user for positive integer
     int n;
     do
     {
         n = get_int("Size: ");
     }
     while (n < 1);
-
+    
+    // print for
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
