db76eec0eab7ed76476d2c4ca8dff02a5bf08cda
LuizHenrique111 <luizhenriquecjz2007@gmail.com>
2024-11-21 19:18:09 +0000

    Thu, Nov 21, 2024, 4:18 PM -03:00

--- mario.c
+++ mario.c
@@ -3,8 +3,13 @@
 
 int main(void)
 {
-    
-    const int n = get_int("Size: ");
+    int n;
+    do
+    {
+        n = get_int("Size: ");
+    }
+    while (n < 1);
+
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
