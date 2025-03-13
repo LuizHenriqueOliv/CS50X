a33d4a3318ed785dbf31be9e87f9558cdb98ee9a
Luiz Henrique <luizhenriquecjz2007@gmail.com>
2025-03-13 17:59:50 +0000

    Thu, Mar 13, 2025, 2:59 PM -03:00

---
+++ week2/problemset/substitution.c
@@ -0,0 +1,24 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, string argv[])
+{
+    // Pegar a chave
+    string key = argv[1];
+    // validar a chave
+        // verificar se tem 26 caracteres
+        if (strlen(key) != 26)
+        {
+            printf("The key dont have 26 caracteres"\n);
+        }
+        // verificar se são todos alphabeticos
+
+        // verificar se são todos diferentes
+    // pegar o texto
+
+    // encriptar
+
+    // printar o texto cifrado
+}
