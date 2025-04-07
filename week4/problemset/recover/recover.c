603e2f2c1d84b6ab52e3b080a6a6c2a507d7bc19
Luiz Henrique <luizhenriquecjz2007@gmail.com>
2025-04-07 17:54:32 +0000

    Mon, Apr 7, 2025, 2:54 PM -03:00

--- week4/problemset/recover/recover.c
+++ week4/problemset/recover/recover.c
@@ -3,12 +3,16 @@
 
 int main(int argc, char *argv[])
 {
-    // accept a single comand-line argument
+    // aceitar uma única linha de comando
     if (argc != 2)
     {
         printf("Please enter one, only one, argument on the command line.\n");
     }
 
-    // open the memory card
-    
+    // abrir o cartão de memória
+
+    // enquanto houver dados para ler no cartão
+
+    // criar JPEGs dos dados
+
 }
