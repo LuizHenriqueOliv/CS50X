67b6c21cbc5bec40581824174ef7b304a42f2791
Luiz Henrique <luizhenriquecjz2007@gmail.com>
2025-05-06 16:15:29 +0000

    Tue, May 6, 2025, 1:15 PM -03:00

--- week5/problemset/speller/dictionary.c
+++ week5/problemset/speller/dictionary.c
@@ -35,7 +35,6 @@ unsigned int hash(const char *word)
 // Loads dictionary into memory, returning true if successful, else false
 bool load(const char *dictionary)
 {
-    
     // TODO
     return false;
 }
