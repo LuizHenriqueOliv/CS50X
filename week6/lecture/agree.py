386c8cf2ec725d2a57fbeb42a41a7146991d46ba
Luiz Henrique <luizhenriquecjz2007@gmail.com>
2025-05-21 18:23:51 +0000

    Wed, May 21, 2025, 3:23 PM -03:00

---
+++ week6/lecture/agree.py
@@ -0,0 +1,6 @@
+s = input("Do you agree? ")
+
+if s in ["y", "Y", "Yes", "YES", "yes"]:
+    print("Agreed.")
+elif s in ["n", "N", "No", "NO", "no"]:
+    print("Not agreed.")
---
+++ week6/lecture/compare.py
@@ -0,0 +1,7 @@
+s = input("S: ")
+t = input("T: ")
+
+if s == t:
+    print("Igual")
+else:
+    print("Diferente")
