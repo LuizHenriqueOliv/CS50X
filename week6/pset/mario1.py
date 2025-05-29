344fe9b135c11abf5e7dcae30b7371d2bbeb0cf8
Luiz Henrique <luizhenriquecjz2007@gmail.com>
2025-05-29 20:11:24 +0000

    Thu, May 29, 2025, 5:11 PM -03:00

--- week6/pset/mario1.py
+++ week6/pset/mario1.py
@@ -4,7 +4,7 @@ from cs50 import get_int
 
 while True:
     height = get_int("Height: ")
-    if (height >= 0 and height <= 8):
+    if (height > 0 and height <= 8):
         break
 
 for i in range(height):
