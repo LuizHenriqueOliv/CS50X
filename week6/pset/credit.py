6ac2691298f6d9cef82ce170dfde8b1bea883269
Luiz Henrique <luizhenriquecjz2007@gmail.com>
2025-05-29 22:08:45 +0000

    Thu, May 29, 2025, 7:08 PM -03:00

--- week6/pset/credit.py
+++ week6/pset/credit.py
@@ -4,7 +4,7 @@ from cs50 import get_string
 card_number = get_string("Informe a sequência do cartão: ")
 
 # verificar pelo algoritmo de Luhn
-length = len(card_number)
-
+for i in card_number:
+    print(card_number[i])
 
 # verificar a bandeira do cartão
