4d42d562145947dbe7855d1261a481a4ae7f9acf
Luiz Henrique <luizhenriquecjz2007@gmail.com>
2025-05-29 20:37:21 +0000

    Thu, May 29, 2025, 5:37 PM -03:00

--- week6/pset/mario.py
+++ week6/pset/mario.py
@@ -1,7 +1,5 @@
 from cs50 import get_int
 
-
-
 while True:
     height = get_int("Height: ")
     if (height > 0 and height <= 8):
@@ -9,5 +7,7 @@ while True:
 
 for i in range(height):
     print(" " * (height - i - 1), end="")
+    print("#" * (i + 1), end="")
+    print("  ", end="")
     print("#" * (i + 1), end="")
     print()
