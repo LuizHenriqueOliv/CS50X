42980cf175bc29751f094d89cb0702347e82b82d
Luiz Henrique <luizhenriquecjz2007@gmail.com>
2025-05-29 21:08:59 +0000

    Thu, May 29, 2025, 6:08 PM -03:00

--- week6/pset/cash.py
+++ week6/pset/cash.py
@@ -8,15 +8,19 @@ while True:
 change *= 100
 
 n_25c = change / 25
+print(n_25c)
 change -= n_25c * 25
 
 n_10c = change / 10
+print(n_10c)
 change -= n_10c * 10
 
 n_5c = change / 5
+print(n_5c)
 change -= n_5c * 5
 
 n_1c = change / 1
+print(n_1c)
 
 total = n_25c + n_10c + n_5c + n_1c
 
