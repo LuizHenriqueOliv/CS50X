820cf1414d08e95ef364d2fbf36bd0999c267cda
Luiz Henrique <luizhenriquecjz2007@gmail.com>
2025-05-29 22:25:54 +0000

    Thu, May 29, 2025, 7:25 PM -03:00

--- week6/pset/credit.py
+++ week6/pset/credit.py
@@ -2,12 +2,16 @@ from cs50 import get_string
 
 # coletar a sequência do cartão
 card_number = get_string("Informe a sequência do cartão: ")
-
+soma = 0
 # verificar pelo algoritmo de Luhn
-for i in range(len(card_number) - 1, 0, -2):
+for i in range(len(card_number) - 2, 0, -2):
     digit = int(card_number[i]) * 2
     if digit > 9:
-        
+        digit1 = digit / 10
+        digit2 = digit % 10
+        soma += digit1 + digit2
+    else:
+        soma += digit
 
-
+for i in range(len(card_number))
 # verificar a bandeira do cartão
