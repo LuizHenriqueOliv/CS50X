9d99b92cfca8bbef013c23e91aeb7f033926e0eb
Luiz Henrique <luizhenriquecjz2007@gmail.com>
2025-08-04 21:01:50 +0000

    Mon, Aug 4, 2025, 6:01 PM -03:00

--- project/app.py
+++ project/app.py
@@ -6,7 +6,7 @@ from cs50 import SQL
 app = Flask(__name__)
 
 # session config
-app.config["SESSION PERMANENT"] = False
+app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
@@ -50,7 +50,7 @@ def register():
 
 @app.route("/login")
 def login():
-    Session.clear()
+    session.clear()
 
     if request.method == "POST":
         return "TODO"
