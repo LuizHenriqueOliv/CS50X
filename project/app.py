7ca139ee38bf6dd6d4534e0ddf26efc72167c245
Luiz Henrique <luizhenriquecjz2007@gmail.com>
2025-08-04 21:53:58 +0000

    Mon, Aug 4, 2025, 6:53 PM -03:00

--- project/app.py
+++ project/app.py
@@ -3,14 +3,12 @@ from flask_session import Session
 from werkzeug.security import generate_password_hash, check_password_hash
 from cs50 import SQL
 from helpers import login_required
-import secrets
 
 app = Flask(__name__)
 
 # session config
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
-app.secret_key = secrets.token_hex(16)
 Session(app)
 
 # connect database
